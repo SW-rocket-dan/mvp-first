0ef8852924fec94fd45a61872d1136b4a784c537
yui880 <kimyuna521@gmail.com>
2024-05-10 16:24:03 +0900

    fix(App): tailwind 경로 문제 해결

--- mvp/tailwind.config.js
+++ mvp/tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
