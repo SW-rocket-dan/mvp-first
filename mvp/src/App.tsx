6238ab8d4d43de30c2e51e72d4b83eb85ef3273c
yui880 <kimyuna521@gmail.com>
2024-05-14 15:26:36 +0900

    feat(App): 제작 폼 링크 연결

--- mvp/src/App.tsx
+++ mvp/src/App.tsx
@@ -8,21 +8,26 @@ import HoverBox from "./components/HoverBox.tsx";
 
 function App() {
 
+    const handleClick = () => {
+        window.open('https://forms.gle/1SZsPLbhtRu9mUDX8');
+    }
+
   return (
     <div className='flex w-full h-full flex-col gap-7'>
       <div className='flex justify-start px-14 pt-10 gap-3'>
           <LogoSvg />
           <p className='text-xl'>로켓단</p>
       </div>
+
         <div className='flex h-full justify-center items-center'>
             <div className=' w-10/12 bg-primary-background rounded-xl'>
                 <div className='flex flex-col justify-center items-center px-12 py-24 gap-5'>
                     <p className='text-7xl font-extrabold'>FAST, EASY, SIMPLE</p>
                     <p className='text-3xl'>AI와 카드뉴스가 만나 더욱 편리하고 효율적으로</p>
-                    <div className='flex flex-row items-center bg-black py-4 px-7 rounded-lg gap-2 mt-7'>
+                    <button className='flex flex-row items-center bg-black py-4 px-7 rounded-lg gap-2 mt-7 shadow-md' onClick={handleClick}>
                         <StarSvg width={25}/>
                         <span className='text-white font-medium'>AI로 카드뉴스 만들기</span>
-                    </div>
+                    </button>
                     <div className='flex flex-row mt-10 gap-16 items-center'>
                         <PromptBox width={440} text='아프리카 농민들이 따온 원두로 눈 앞에서 착즙하는 커피 자판기 체인점이 아닌, 깔끔하고 친숙, 저렴한 일상적인 커피' purpose='커피 자판기 홍보' />
                         <GroupSVG />
